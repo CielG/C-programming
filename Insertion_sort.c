0b52dbad4b08eb2df6c7d4acf60b05e0ae83995e
CielG <Ciel18181919@outlook.com>
2016-11-22 13:22:10 +0800

    Add files via upload

---
+++ Insertion_sort.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void Insertion_sort(int A[],int n);
+
+void main()
+{
+  int i,n;
+   int a[20];
+
+  printf("Enter the size of your array\n");
+  scanf("%d",&n);
+
+  printf("Enter your array \n");
+  for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+  Insertion_sort(a,n);
+
+  for(i=0;i<n;i++)
+    {
+      printf("%d\n",a[i]);
+    }
+
+}
+void Insertion_sort (int A[],int n)
+{
+  int i,j,key,temp;
+  for(i=1; i<n; i++)
+    {
+      j=i;
+      key=A[i];
+      while(j>0 && A[j-1]>A[j])
+	{
+	  temp=A[j];
+	  A[j]=A[j-1];
+	  A[j-1]=temp;
+	  j--;
+	}
+      A[j]=key;
+    }
+}
---
+++ Merge sort.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+void merge(int A[],int T[],int start,int mid,int end)
+{
+    int i=start,j=mid+1;
+    int k=0;
+    while(i<=mid && j<=end)
+    {
+        if(A[i]<=A[j])
+        {
+            T[k++]=A[i++];
+        }
+        else
+        {
+            T[k++]=A[j++];
+        }
+    }
+    while(i<=mid)
+    {
+        T[k++]=A[i++];
+    }
+    while(j<=end)
+    {
+        T[k++]=A[j++];
+    }
+    for(i=start,k=0;i<=end;i++,k++)
+    {
+        A[i]=T[k];
+    }
+}
+void mergesort(int a[],int T[],int start,int end)
+{
+    if(start<end)
+    {
+        int mid=(start+end)/2;
+        mergesort(a,T,start,mid);
+        mergesort(a,T,mid+1,end);
+        merge(a,T,start,mid,end);
+    }
+}
+
+void main()
+{
+    int n,i,j,a[100],t[100];
+
+    printf("Enter the size of your array \n");
+    scanf("%d",&n);
+
+    printf("Enter your array \n");
+
+    for (i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    mergesort(a,t,0,n-1);
+    for (i=0; i<n; i++)
+    {
+        printf("%d\n",a[i]);
+
+    }
+}
