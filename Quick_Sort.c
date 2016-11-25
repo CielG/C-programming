5ec6104b45250efe158b9fa735fb82b31d05b17f
CielG <Ciel18181919@outlook.com>
2016-11-25 20:04:15 +0800

    Update Quick_Sort.c

--- Quick_Sort.c
+++ Quick_Sort.c
@@ -33,7 +33,7 @@ void quicksort(int A[],int l, int r)
     }
 }
 
-void main()
+int main()
 {
     int n,i,j,a[100];
 
@@ -52,8 +52,8 @@ void main()
     for (i=0; i<n; i++)
     {
         printf("%d\n",a[i]);
-
     }
+    return 0;
 }
 
 
